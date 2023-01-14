7ac6fd7d554efc94cf13d6a8a063e2ea9a5ec88a
scichocka94 <117513470+scichocka94@users.noreply.github.com>
2023-01-14 14:54:28 +0100

    zmiana settings dotven

--- projekt1/projekt1/settings.py
+++ projekt1/projekt1/settings.py
@@ -18,6 +18,7 @@ import dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+dotenv.load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
